f61038326e8f1a444f368548fa3dd707dbb4d1d5
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-04-10 15:15:10 -0400

    WIP

--- Package.ps1
+++ Package.ps1
@@ -1,15 +1,16 @@
 # Creates the release's .zip file
 
-$ModName = "Guil-自然颂歌";
+$ModName = "Guil-自然颂歌"
 $ModFolder = "./Package/" + $ModName
 $ArchiveName = "Guil-Nature.zip"
-
 
 Remove-Item -ErrorAction SilentlyContinue -Recurse ./Package/*
 Remove-Item -ErrorAction SilentlyContinue $ArchiveName
 mkdir -ErrorAction SilentlyContinue $ModFolder
 
-Copy-Item -Recurse ./$ModName/* $ModFolder
+dotnet publish .\src\自然颂歌.csproj -o $ModFolder -c Release
+
+Copy-Item -Recurse "./$ModName/*" $ModFolder
 
 # English name since github strips Unicode for security purposes.
 Compress-Archive -DestinationPath $ArchiveName -Path ./Package/*
